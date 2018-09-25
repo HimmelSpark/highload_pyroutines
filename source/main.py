e0e2c65637af537e75e6a1f0cb33dccc122f6169
HimmelSpark <adam404pet@gmail.com>
2018-09-25 18:53:16 +0300

    removed comments

--- source/main.py
+++ source/main.py
@@ -12,39 +12,6 @@ from handler.handler import Handler
 from server.server import Server
 
 procs = []
-# MAXIMMUM ULIMIT ON MY MAC IS 12288
-
-# forks = []
-# if __name__ == '__main__':
-#     config = ConfigParser.parse()
-#
-#     # logger = logging.getLogger('server')
-#
-#     # asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-#
-#     for i in range(0, int(config.cpu_count)):
-#         pID = os.fork()
-#         forks.append(pID)
-#
-#         if pID == 0:
-#             loop = asyncio.get_event_loop()
-#             print('running source with PID: {0}'.format(str(os.getpid())))
-#             for _ in range(0, int(config.threads)):
-#                 loop.create_task(
-#                     Server(
-#                         config,
-#                         loop,
-#                         Handler(config.root_dir, Executor(config)),
-#                         )
-#                     .launch_server()
-#                 )
-#                 loop.run_forever()
-#
-#     for pID in forks:
-#         os.waitpid(pID, 0)
-
-#
-
 
 def start(conf, sock):
     loop = asyncio.get_event_loop()
@@ -83,7 +50,6 @@ if __name__ == '__main__':
             i.start()
 
         for i in procs:
-            # i.join()
             os.waitpid(i.pid, 0)
 
     except KeyboardInterrupt:
